ab485e0189d8c5f4cd1522554ea8db7386894592
ThuthuzaPoswayo <167189199+ThuthuzaPoswayo@users.noreply.github.com>
2024-06-04 15:50:42 +0200

    Add files via upload

---
+++ AgetoDays/script/code .js
@@ -0,0 +1,7 @@
+function convertToSeconds() {
+    let minutes = +document.getElementById("conversion").value;
+    let Days= Age * 365
+
+    document.getElementById('results').innerText = dsyd + "age is equal to" + days
+
+}
